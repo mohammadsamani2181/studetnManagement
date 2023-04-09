e070178d72a376149c1dc8795ccf11a211eb30cb
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-09 18:18:52 +0430

    save student and getAllStudents methods added

---
+++ src/main/java/com/studentManagement/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.studentManagement.controller;
+
+import com.studentManagement.model.Student;
+import com.studentManagement.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Student> saveStudent(Student student) {
+        return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+}
