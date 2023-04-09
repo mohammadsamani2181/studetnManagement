0d8ca12bb72a4c292ef0d62d71f54cde00e3b4a8
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-09 18:18:07 +0430

    saveStudent and getAllStudents methods added

---
+++ src/main/java/com/studentManagement/service/impl/StudentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.studentManagement.service.impl;
+
+import com.studentManagement.model.Student;
+import com.studentManagement.repository.StudentRepository;
+import com.studentManagement.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
