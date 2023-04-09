c436f5c0659abc7b6b93c919772df30a7b342f38
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-09 21:28:31 +0430

    getStudentById, updateStudent, and deleteStudentById methods added

--- src/main/java/com/studentManagement/controller/StudentController.java
+++ src/main/java/com/studentManagement/controller/StudentController.java
@@ -4,10 +4,7 @@ import com.studentManagement.model.Student;
 import com.studentManagement.service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,22 @@ public class StudentController {
     @GetMapping
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Student> getStudentById (@PathVariable(value = "id") Long id) {
+        return new ResponseEntity<>(studentService.getStudentById(id), HttpStatus.OK);
+    }
+
+
+    @PutMapping("{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable(value = "id") Long id, @RequestBody Student newStudent) {
+        return new ResponseEntity<>(studentService.updateStudent(id, newStudent), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteStudentById (@PathVariable(value = "id") Long id) {
+        studentService.deleteStudentById(id);
+        return new ResponseEntity<>("Successfully Deleted", HttpStatus.OK);
     }
 }
