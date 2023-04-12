1b43a664c3b9186688a92f9d0e95d492de6d72df
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-12 10:04:27 +0430

    a new method added to get all the student from database using @Query

--- src/main/java/com/studentManagement/repository/StudentRepository.java
+++ src/main/java/com/studentManagement/repository/StudentRepository.java
@@ -2,6 +2,15 @@ package com.studentManagement.repository;
 
 import com.studentManagement.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("select s from Student s")
+    public List<Student> findAllStudents();
+
 }
