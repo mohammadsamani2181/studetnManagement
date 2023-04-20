a348f9e71fda109c5eb1956c45d404547af56cf0
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-20 17:49:51 +0430

    created

---
+++ src/main/java/com/studentManagement/repository/SchoolRepository.java
@@ -0,0 +1,9 @@
+package com.studentManagement.repository;
+
+import com.studentManagement.model.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long> {
+}
