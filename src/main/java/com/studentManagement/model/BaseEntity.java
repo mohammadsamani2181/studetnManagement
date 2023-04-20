a5b32b2e2ca178a517de4779309816ee6e8ec84a
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-20 17:44:43 +0430

    created and some fields added so all the entities can inherit from it

---
+++ src/main/java/com/studentManagement/model/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.studentManagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_time", nullable = false)
+    private LocalDateTime createdDateTime;
+
+    @UpdateTimestamp
+    @Column(name = "updated_time", nullable = false)
+    private LocalDateTime updatedDateTime;
+}
