68cb54ae7a48e207795f8bd57d6d22f477c9a749
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-20 17:51:52 +0430

    some fields removed and Student inherits from BaseUser. some fields added. also some annotations for having relationship with other classes added.

--- src/main/java/com/studentManagement/model/Student.java
+++ src/main/java/com/studentManagement/model/Student.java
@@ -1,36 +1,59 @@
 package com.studentManagement.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-@Data
 @Entity
-@Table(name = "students")
-public class Student {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "first_name", nullable = false)
-    private String firstName;
-    @Column(name = "last_name", nullable = false)
-    private String lastName;
-    @Column(name = "email", nullable = false)
-    private String email;
+@Table(name = "studentM_Student")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student extends BaseUser{
     @Column(name = "level", nullable = false)
     @Enumerated(value = EnumType.ORDINAL)
     private StudentLevel studentLevel;
+
     @Column(name = "score", nullable = false)
     private int score;
-    @Column
-    @CreationTimestamp
-    private LocalDateTime createDateTime;
-    @Column
-    @UpdateTimestamp
-    private LocalDateTime updateDateTime;
+
+    @ManyToMany(targetEntity = Lesson.class,
+                fetch = FetchType.LAZY,
+                cascade = {CascadeType.DETACH, CascadeType.PERSIST})
+
+    @JoinTable(name = "studentM_student_lesson",
+            joinColumns = {
+                    @JoinColumn(name = "student_id")/*,
+                    @JoinColumn(name = "student_firstName"),
+                    @JoinColumn(name = "student_lastName")*/
+                    },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "lesson_id")/*,
+                    @JoinColumn(name = "lesson_subject")*/
+            })
+    private List<Lesson> lessons = new ArrayList<>();
+
+
+    @ManyToOne(targetEntity = Teacher.class,
+               fetch = FetchType.LAZY,
+               cascade = {CascadeType.DETACH, CascadeType.PERSIST})
+    private Teacher teacher;
+
+    @ManyToOne(targetEntity = School.class,
+               fetch = FetchType.LAZY,
+               cascade = CascadeType.PERSIST)
+    private School school;
+
+
+    public Student(String firstName, String lastName, String email, StudentLevel studentLevel) {
+        super(firstName, lastName, email);
+        this.studentLevel = studentLevel;
+    }
 }
