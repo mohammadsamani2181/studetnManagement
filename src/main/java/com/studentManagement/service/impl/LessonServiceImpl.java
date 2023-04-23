c6a8d6c807717636efcd1f9dfd2c71a653c82953
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-24 00:26:08 +0430

    saveLesson method added

---
+++ src/main/java/com/studentManagement/service/impl/LessonServiceImpl.java
@@ -0,0 +1,29 @@
+package com.studentManagement.service.impl;
+
+import com.studentManagement.model.DTO.request.LessonDTORequest;
+import com.studentManagement.model.DTO.response.LessonDTOResponse;
+import com.studentManagement.model.Lesson;
+import com.studentManagement.model.SchoolConfig;
+import com.studentManagement.repository.LessonRepository;
+import com.studentManagement.service.LessonService;
+import com.studentManagement.service.mapper.LessonDTORequestMapper;
+import com.studentManagement.service.mapper.LessonDTOResponseMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LessonServiceImpl implements LessonService {
+    private LessonRepository lessonRepository;
+    private LessonDTORequestMapper lessonDTORequestMapper;
+    private LessonDTOResponseMapper lessonDTOResponseMapper;
+    public LessonServiceImpl(LessonRepository lessonRepository, LessonDTORequestMapper lessonDTORequestMapper, SchoolConfig schoolConfig, LessonDTOResponseMapper lessonDTOResponseMapper) {
+        this.lessonRepository = lessonRepository;
+        this.lessonDTORequestMapper = lessonDTORequestMapper;
+        this.lessonDTOResponseMapper = lessonDTOResponseMapper;
+    }
+
+    @Override
+    public LessonDTOResponse saveLesson(LessonDTORequest lessonDTORequest) {
+        Lesson lesson = lessonDTORequestMapper.apply(lessonDTORequest);
+        return lessonDTOResponseMapper.apply(lessonRepository.save(lesson));
+    }
+}
