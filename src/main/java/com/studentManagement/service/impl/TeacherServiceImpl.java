3b30f6d9a7825cf90434be4a9477cd7c38eb540a
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-24 00:28:00 +0430

    created

---
+++ src/main/java/com/studentManagement/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,9 @@
+package com.studentManagement.service.impl;
+
+import com.studentManagement.service.TeacherService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeacherServiceImpl implements TeacherService {
+
+}
