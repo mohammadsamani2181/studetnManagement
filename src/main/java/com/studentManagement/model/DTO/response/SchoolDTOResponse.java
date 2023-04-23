262212bdea158d9ae60dfac835793cec0ac6db52
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-24 00:11:50 +0430

    added for school response

---
+++ src/main/java/com/studentManagement/model/DTO/response/SchoolDTOResponse.java
@@ -0,0 +1,15 @@
+package com.studentManagement.model.DTO.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SchoolDTOResponse {
+    private Long id;
+    private String name;
+}
