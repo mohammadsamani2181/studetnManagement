6b9451be929b3776d8299092466663c8a364a326
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-27 15:57:06 +0430

    created for Teacher update requests

---
+++ src/main/java/com/studentManagement/model/DTO/request/TeacherDTOUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.studentManagement.model.DTO.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeacherDTOUpdateRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
