a9562c3f0c916d52f9d4c6784775a185f02c6a87
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-27 15:37:40 +0430

    using builder method added

--- src/main/java/com/studentManagement/service/mapper/LessonDTOResponseMapper.java
+++ src/main/java/com/studentManagement/service/mapper/LessonDTOResponseMapper.java
@@ -12,12 +12,17 @@ import java.util.function.Function;
 public class LessonDTOResponseMapper implements Function<Lesson, LessonDTOResponse> {
     @Override
     public LessonDTOResponse apply(Lesson lesson) {
-        return new LessonDTOResponse(
-             lesson.getId(),
-             lesson.getType(),
-             lesson.getSubject()
-        );
+        return LessonDTOResponse.builder()
+                .id(lesson.getId())
+                .type(lesson.getType())
+                .subject(lesson.getSubject())
+                .build();
     }
+
+    public List<LessonDTOResponse> applyAll(Set<Lesson> lessons) {
+        if (lessons == null) {
+            return null;
+        }
 
     public List<LessonDTOResponse> applyAll(List<Lesson> lessons) {
         List<LessonDTOResponse> lessonDTOResponseList = new ArrayList<>();
