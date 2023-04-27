ca39a47cc82252e8f33f5a816001982382b340e3
mmd_s21 <hdhdmohammad5@gmail.com>
2023-04-27 15:54:33 +0430

    saveTeacher, addLessons, deleteLesson, updateTeacher, and deleteTeacher methods added

--- src/main/java/com/studentManagement/controller/TeacherController.java
+++ src/main/java/com/studentManagement/controller/TeacherController.java
@@ -1,9 +1,55 @@
 package com.studentManagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.studentManagement.model.DTO.request.IdDTORequest;
+import com.studentManagement.model.DTO.request.TeacherDTOSaveRequest;
+import com.studentManagement.model.DTO.request.TeacherDTOUpdateRequest;
+import com.studentManagement.model.DTO.response.TeacherDTOResponse;
+import com.studentManagement.service.TeacherService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/school/teachers")
+@Validated
 public class TeacherController {
+    private TeacherService teacherService;
+
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TeacherDTOResponse> saveTeacher(@RequestBody @Valid TeacherDTOSaveRequest teacherDTOSaveRequest) {
+        return new ResponseEntity<>(teacherService.saveTeacher(teacherDTOSaveRequest), HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{teacherId}/lessons")
+    public ResponseEntity<TeacherDTOResponse> addLessons(@RequestBody IdDTORequest lessonsIdList,
+                                                         @PathVariable("teacherId") Long teacherId) {
+
+        return new ResponseEntity<>(teacherService.addLessons(lessonsIdList, teacherId), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{teacherId}/lessons/{lessonId}")
+    public ResponseEntity<TeacherDTOResponse> deleteLesson(@PathVariable("lessonId") Long lessonId,
+                                                            @PathVariable("teacherId") Long teacherId) {
+
+        return new ResponseEntity<>(teacherService.deleteLesson(lessonId, teacherId), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{teacherId}")
+    public ResponseEntity<TeacherDTOResponse> updateTeacher(@RequestBody TeacherDTOUpdateRequest teacherDTOUpdateRequest,
+                                                           @PathVariable("teacherId") Long teacherId) {
+
+        return new ResponseEntity<>(teacherService.updateTeacher(teacherDTOUpdateRequest, teacherId), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{teacherId}")
+    public ResponseEntity<String> deleteTeacher( @PathVariable("teacherId") Long teacherId) {
+        teacherService.deleteTeacher(teacherId);
+        return new ResponseEntity<>("Successfully Deleted", HttpStatus.OK);
+    }
 }
